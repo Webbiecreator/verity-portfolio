1431f80ba0cfa8c4f7f8089cf6995c238fa3e15e
Webbiecreator <gauravspradhan@gmail.com>
2026-08-21 00:25:24 +0530

    chore: remove experimental Verity Engine section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Preloader from "@/components/common/preloader";
 import OpeningScrollytelling from "@/components/sections/home/opening-scrollytelling";
 import CollabSec from "@/components/sections/home/collab-section";
 import BuildingLightsSection from "@/components/sections/about/building-lights-section";
-import VerityEngineSection from "@/components/sections/home/verity-engine-section";
 import CalBooking from "@/components/sections/home/cal-booking";
 
 export default function Home() {
@@ -31,8 +30,6 @@ export default function Home() {
       <section id="about" className="w-full">
         <BuildingLightsSection />
       </section>
-
-      <VerityEngineSection />
 
       <section id="projects" className="w-full scroll-mt-24">
         <TimelineDemo />
